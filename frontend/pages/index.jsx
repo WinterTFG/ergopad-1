c9f0a79e893fb8c24d70fbdc9aacfa3cd2a3b047
esot321c <esot321c@gmail.com>
2021-11-28 16:17:34 -0800

    tentative timeline added to front page

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,6 +1,8 @@
 import Features from '@components/Features';
 import Hero from '@components/Hero';
-import { Container } from '@mui/material';
+import { Container, Divider } from '@mui/material';
+import RoadMap from '@components/RoadMap';
+import CenterTitle from '@components/CenterTitle'
 
 const Homepage = () => {
   return (
@@ -11,7 +13,18 @@ const Homepage = () => {
           subtitle='We are a token launch platform for Ergo giving you an opportunity to get in on the ground floor with Ergo token IDOs. We help projects navigate Ergoscript to build safe apps for you to invest in.'
         />
 
+        <Divider sx={{ mb: 10 }} />
+
         <Features />
+
+        <Divider sx={{ mb: 10 }} />
+
+        <CenterTitle
+          title="Roadmap"
+          subtitle="ErgoPad's tentative launch schedule"
+          sx={{ pt: '5rem' }}
+        />
+        <RoadMap />
       </Container>
     </>
   );
