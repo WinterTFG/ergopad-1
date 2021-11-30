98edeb807a0cff3395364ba984170f2036b667ca
esot321c <esot321c@gmail.com>
2021-11-30 12:46:09 -0800

    update links

--- frontend/components/Hero.jsx
+++ frontend/components/Hero.jsx
@@ -51,7 +51,7 @@ const Hero = ({ title, subtitle }) => {
             </Button>
           </a>
 
-          <a href="https://discord.gg/M9VJ9XUMuA" target="_blank">
+          <a href="https://discord.gg/E8cHp6ThuZ" target="_blank">
             <Button 
               variant="contained"
               sx={{
