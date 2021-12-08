43677fc1c1ee581927e356fd129a2c80b7c331b0
esot321c <esot321c@gmail.com>
2021-12-07 20:13:50 -0800

    add whitelist page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   env: {
     // CHANGE FOR PRODUCTION VERSION
     // API_URL: 'https://ergopad.io/api',
-    API_URL: 'http://localhost:8000/api',
+    API_URL: 'http://192.168.1.80:8000/api',
   },
 }
